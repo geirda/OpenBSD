35d8de495ee3ff443ac3127a391e8a9372be2a18
Geir Damstuen <geirda@gmail.com>
2019-09-04 22:35:57 +0200

    Add files

--- bin/statusbar.sh
+++ bin/statusbar.sh
@@ -2,7 +2,7 @@
 
 while true;
 do
-    ip=$(ifconfig|head -13|tail -1|awk '{print $2}')
+    ip=$(ifconfig|awk '/broadcast/{print $2}')
     homefs=$(df -h|awk '/sd0k/{print $4}')
     localfs=$(df -h||awk '/sd0h/{print $4}')
     dato=$(date '+%a %d %b %H:%M')
