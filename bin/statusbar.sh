7832c5f28d964363074538981cd3148a24ca13b6
Geir Damstuen <geirda@gmail.com>
2019-09-04 22:33:00 +0200

    Add files

--- bin/statusbar.sh
+++ bin/statusbar.sh
@@ -3,8 +3,8 @@
 while true;
 do
     ip=$(ifconfig|head -13|tail -1|awk '{print $2}')
-    homefs=$(df -h|tail -4|head -1|awk '{print $4}')
-    localfs=$(df -h|tail -2|head -1|awk '{print $4}')
+    homefs=$(df -h|awk '/sd0k/{print $4}')
+    localfs=$(df -h||awk '/sd0h/{print $4}')
     dato=$(date '+%a %d %b %H:%M')
     xsetroot -name " /home $homefs /usr/local $localfs    IP: $ip    $dato "
     sleep 5;
