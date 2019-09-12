2332017eab314c9559f7f9570cc96c789f5be31a
Geir Damstuen <geirda@gmail.com>
2019-09-12 08:23:44 +0200

    Add files

--- .dwm/dwmconky.sh
+++ .dwm/dwmconky.sh
@@ -4,4 +4,4 @@
  BG='#0a0a0a'
  FONT="Hack:size=11:antialias=true:hinting=true"
 # FONT='-*-terminus-*-r-normal-*-*-80-*-*-*-*-iso8859-*'
- conky --config=$HOME/.dwmconkyrc | dzen2 -e - -x '1400' -h '24' -w '1160' -ta r -fg $FG -bg $BG -fn $FONT &
+ conky --config=$HOME/.config/dwmconkyrc | dzen2 -e - -x '1400' -h '24' -w '1160' -ta r -fg $FG -bg $BG -fn $FONT &
