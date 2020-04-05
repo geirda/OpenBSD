80999390c5214b868f72a5d1b46ea9060e4e22ab
Geir Damstuen <geirda@gmail.com>
2020-04-05 14:11:52 +0200

    Small changes

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 nitrogen --restore &
 compton --config $HOME/.config/compton-dwm.conf &
-$HOME/.dwm/dwmconky.sh &
-#$HOME/.dwm/statusbar.sh 2>&1 > /dev/null &
+$HOME/.config/dwm/dwmconky.sh &
+#$HOME/.config/dwm/statusbar.sh 2>&1 > /dev/null &
 xset m 10 1 &
